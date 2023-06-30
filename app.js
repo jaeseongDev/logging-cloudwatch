7a87889c3efda694d502ff0531f1eac1183dce83
jaeseongdev <qkrwotjd1445@naver.com>
2023-06-30 14:09:01 +0900

    feat: 에러 로깅 남기기

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('./utils/logger')
 app.get('/', (req, res) => {
   logger.info(new Date().getTime() + 'hello!');
   logger.error(new Date().getTime() + 'error!!');
+  res.send(new Date().getTime().toString());
 })
 
 app.listen(port, () => {
